a929667af43426ccb926b3e075c8c36297c15039
unknown <ivanastankovski91@gmail.com>
2019-08-28 18:26:01 +0200

    add all

---
+++ js/mobileFirst.js
@@ -0,0 +1,11 @@
+//Animate smooth scrolle
+$('#view-work').on('click', function() {
+    const images = $('#images').position().top;
+
+    $('html, body').animate(
+        {
+            scrollTop: images
+        }, 
+        900
+    );
+});
